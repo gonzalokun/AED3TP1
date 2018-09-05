118a92c2c625e44e8eeb4c284426f79ea742141a
Gonzalo Springhart <glspringhart@gmail.com>
2018-09-05 16:23:07 -0300

    primer commit

---
+++ src/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+//Comentar linea: ctrl-shift-c, descomentar: ctrl-shift-x
+
+//(Por ahora) Todo el código se encuentra en el mismo archivo
+// para facilitar lectura/localizacion del código
+
+//Código de Input/Output (IO)
+//Fin código IO
+
+//Código de Fuerza Bruta (FB)
+
+//Generador de conjunto partes
+vector<vector<int>> conjuntoPartes(vector<int> conjuntoInicial){
+    vector<vector<int>> conjuntoFinal;
+    return conjuntoFinal;
+}
+//
+
+//Fin código FB
+
+//Código de Backtracking (BT)
+//Fin código BT
+
+//Código de Programación Dinámica (PD)
+//Fin código PD
+
+int main()
+{
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
