f3b4a7291caab4cc86b5861cb0dda85fa632e0aa
Gonzalo Springhart <glspringhart@gmail.com>
2018-09-08 01:48:46 -0300

    Borrada funcion que no funcionaba

--- src/main.cpp
+++ src/main.cpp
@@ -78,41 +78,25 @@ int subsetSumFuerzaBruta(std::vector<int> conjuntoInicial, int valorObjetivo){
 
 //Vamos probando con todo hasta encontrar uno que vaya
 
-int subsetSumBacktracking(std::vector<int>& conjuntoInicial, int valorObjetivo){ //O(n^3) (Por ahora)
-    //std::vector<int> conjResultado; //quiza no sea necesario
-    int longMin = -1;
-    //int suma = 0;
-
-    //Ordeno el conjunto de numeros de forma creciente
-    std::sort(conjuntoInicial.begin(), conjuntoInicial.end());
-
-    for(int inicio = 0; inicio < conjuntoInicial.size(); inicio++){ //O(n)
-        //
-        for(int proximo = inicio; proximo < conjuntoInicial.size(); proximo++){ //O(n)
-            //HACER UN WHILE ACA QUE HAGA LA SUMAAAA
-            //
-        }
-        //Reseteo la suma
-    }
-
-    return longMin;
+int subsetSumBacktracking(std::vector<int>& conjuntoInicial, int valorObjetivo){
+    //
 }
 
 //Suma los elementos de conjunto de la siguiente forma:
 //Primero suma posInicial y despues a eso le suma los
 //elementos de conjunto empezando desde sumaDesde hasta
 //sumaHasta
-int sumarSalteando(std::vector<int> conjunto, int posInicial, int sumaDesde, int sumaHasta){
-    int suma = 0;
-    suma += conjunto[posInicial];
-
-    //Empieza por salto
-    for(int elem = sumaDesde; elem < sumaHasta && elem < conjunto.size(); elem++){
-        suma += conjunto[elem];
-    }
-
-    return suma;
-}
+//int sumarSalteando(std::vector<int> conjunto, int posInicial, int sumaDesde, int sumaHasta){
+//    int suma = 0;
+//    suma += conjunto[posInicial];
+//
+//    //Empieza por salto
+//    for(int elem = sumaDesde; elem < sumaHasta && elem < conjunto.size(); elem++){
+//        suma += conjunto[elem];
+//    }
+//
+//    return suma;
+//}
 
 //Fin código BT
 
